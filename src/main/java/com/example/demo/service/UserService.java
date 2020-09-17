a3a5ea027324b9c8f93ef3f5f5f8c5a33e30612e
Tatsiana Rrr <tatsiana.andryievich@gmail.com>
2020-09-17 13:14:41 +0300

    UserService.java add CRUD methods

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -9,10 +9,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
-    Logger logger = LoggerFactory.getLogger(UserService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserDaoI userDaoI;
@@ -22,8 +23,50 @@ public class UserService {
         userDaoI.findAll().forEach(res::add);
         return res;
     }
+
+    public User save(User user) {
+        return userDaoI.save(user);
+    }
+
+    public List<User> saveAll(List<User> list) {
+        userDaoI.saveAll(list).forEach(list::add);
+        return list;
+    }
+
+    public Optional<User> findById(long id) {
+        return userDaoI.findById(id);
+    }
+
+    public boolean existsById(long id) {
+        return userDaoI.existsById(id);
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        userDaoI.findAll().forEach(list::add);
+        return list;
+    }
+
+    public List<User> findAllById(List<Long> list) {
+        List<User> results = new ArrayList<>();
+        list.forEach(id -> findById(id).ifPresent(results::add));
+        return results;
+    }
+
+    public void deleteById(long id) {
+        userDaoI.deleteById(id);
+    }
+
+    public void delete(User user) {
+        userDaoI.delete(user);
+    }
+
+    public void deleteAll(List<User> list) {
+        userDaoI.deleteAll(list);
+    }
 }
-   /* public List<User> getAll() {
+
+       /* public List<User> getAll() {
         List<User> res = new ArrayList<>();
         userDaoI.findAll().forEach(e -> {
             res.add(e);
