58a64ce9599da4d950308486250ff931e19bb28c
Tatsiana Rrr <tatsiana.andryievich@gmail.com>
2020-09-29 16:59:32 +0300

    BookController

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -18,7 +18,7 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     List<Book> getAll() {
         return bookService.getAll();
@@ -29,29 +29,20 @@ public class BookController {
         return bookService.save(book);
     }
 
-    public List<Book> saveAll(List<Book> list) {
-        bookService.saveAll(list);
-        return list;
-    }
-
     @GetMapping("/{bookId}")
     public @ResponseBody
     Optional<Book> findById(@PathVariable("bookId") Integer id) {
         return bookService.findById(id);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable long id) {
         bookService.deleteById(id);
     }
 
-    @DeleteMapping("/delete/{book}")
-    public void delete(@RequestBody Book book) {
-        bookService.delete(book);
-    }
-
-    @DeleteMapping
-    public void deleteAll(@RequestParam List<Book> list) {
-        bookService.deleteAll(list);
+    @PutMapping
+    public Book update (@RequestBody Book book){
+        //bookService.update(book)
+        return book;
     }
 }
